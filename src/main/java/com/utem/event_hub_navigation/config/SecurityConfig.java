cadfebca068960746e3c3690b962803e3be39826
p0205 <kks741224@gmail.com>
2025-06-23 12:00:05 +0800

    Fix issue

--- src/main/java/com/utem/event_hub_navigation/config/SecurityConfig.java
+++ src/main/java/com/utem/event_hub_navigation/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.utem.event_hub_navigation.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.utem.event_hub_navigation.security.JwtAuthenticationFilter;
 import com.utem.event_hub_navigation.security.UserDetailsServiceImpl;
